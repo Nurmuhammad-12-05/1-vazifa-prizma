935f37ec124591519c1408c14413c1b835ee8ced
Web developer <150703003+mirkamolhomidov@users.noreply.github.com>
2025-05-02 15:31:35 +0500

    push

--- src/app.js
+++ src/app.js
@@ -1,28 +1,30 @@
-import "dotenv/config";
-import express from "express";
-import prisma from "./config/prisma.js";
-import Routes from "./routes/routes.js";
-import ErrorMiddleware from "./middlewares/error.niddleware.js";
+import 'dotenv/config'
+import express from 'express'
+import prisma from './config/prisma.js'
+import ErrorMiddleware from './middlewares/error.niddleware.js'
+import Routes from './routes/routes.js'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use("/api", Routes());
-app.use(ErrorMiddleware);
+app.use(express.json())
+app.use('/api', Routes())
+app.use(ErrorMiddleware)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 const initApp = async () => {
   try {
-    await prisma.$connect();
-    console.log("Database connected.");
+    console.log('Salom')
+
+    await prisma.$connect()
+    console.log('Database connected.')
 
     app.listen(PORT, () => {
-      console.log("Server is ranning", PORT);
-    });
+      console.log('Server is ranning', PORT)
+    })
   } catch (error) {
-    console.error(error.message);
+    console.error(error.message)
   }
-};
+}
 
-initApp();
+initApp()
